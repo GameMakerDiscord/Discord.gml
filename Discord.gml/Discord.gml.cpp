61184acf9de9ac90afa3ac8e5fd111a2d667bd3e
luigi <36678917+oluigipo@users.noreply.github.com>
2021-06-08 19:37:51 -0300

    ✔
    I'm dumb

--- Discord.gml/Discord.gml.cpp
+++ Discord.gml/Discord.gml.cpp
@@ -150,7 +150,7 @@ class async_event {
 	}
 	/// Dispatches this event and cleans up the map.
 	void dispatch() {
-		gml_event_perform_async(map, 69);
+		gml_event_perform_async(map, async_event_to_perform);
 	}
 	bool set(char* key, double value) {
 		return gml_ds_map_set_double(map, key, value);
@@ -179,7 +179,7 @@ class async_event {
 	}
 };
 dllx double discord_dispatch_event(double map_id) {
-	gml_event_perform_async((gml_ds_map)map_id, 69);
+	gml_event_perform_async((gml_ds_map)map_id, async_event_to_perform);
 	return true;
 }
 
